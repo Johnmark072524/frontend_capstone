b90620850f98bb37edd293c3df839e98eaa822d6
jm <johnmarkcabauatan17@gmail.com>
2026-09-03 21:19:18 +0800

    some bugs

--- js/config.js
+++ js/config.js
@@ -1,13 +1,10 @@
 // js/config.js
 let API_BASE_URL = "http://localhost:8080";
-let isLocalhost = true;
 
 if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
-  API_BASE_URL = "https://vitamins-april-unify.ngrok-free.dev";
-  isLocalhost = false;
+  API_BASE_URL = "https://backend-hmxf.onrender.com";
 }
 
 const API_HEADERS = {
-  'Content-Type': 'application/json',
-  ...(!isLocalhost && { 'ngrok-skip-browser-warning': '69420' })
+  'Content-Type': 'application/json'
 };
